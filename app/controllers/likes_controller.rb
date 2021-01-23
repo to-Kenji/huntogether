2b00ae7b4eb9f5ff544738726cce7f401a3c1912
to-Kenji <kura101089kenji@gmail.com>
2021-01-23 15:58:56 +0900

    modified design of comments

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,11 +4,19 @@ class LikesController < ApplicationController
 
   def create
     Like.create(user_id: current_user.id, technique_id: params[:id])
+    respond_to do |format|
+      format.html {redirect_to @technique}
+      format.js
+    end
   end
 
   def destroy
     like = Like.find_by(user_id: current_user.id, technique_id: params[:id])
     like.destroy
+    respond_to do |format|
+      format.html {redirect_to @technique}
+      format.js
+    end
   end
 
   private
