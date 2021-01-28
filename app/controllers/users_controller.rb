0e4c87f9bd44702f1ae8394047e838b6926614b4
to-Kenji <kura101089kenji@gmail.com>
2021-01-28 18:00:58 +0900

    followings followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-
+  before_action :set_user, only: [:show, :followings, :followers]
   def show
-    @user = User.find(params[:id])
     @techniques = @user.techniques.recent.page(params[:page]).per(3)
     respond_to do |format|
       format.html
@@ -15,10 +14,23 @@ class UsersController < ApplicationController
     if @q = @favorites.ransack(params[:q])
       @favorites = @q.result.recent.page(params[:page]).per(5)
     end
-
     respond_to do |format|
       format.html
       format.js {render :favorites_paginate}
     end
+  end
+
+  def followings
+    @followings = @user.followings.page(params[:page])
+  end
+
+  def followers
+    @followers = @user.followers.page(params[:page])
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
