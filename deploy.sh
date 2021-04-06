4d5980358ce2358836b3d8a0fc57ad8226ffc51c
to-Kenji <kura101089kenji@gmail.com>
2021-04-06 15:34:53 +0900

    deploy.shの構文ミス修正

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ set -ex
 SECURITY_GROUP_NAME="huntogether_circleci_sg"
 IP=`curl -s ifconfig.me`
 
-aws ec2 authorize-security-group-ingress --group-id ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${IP}}/32
+aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${IP}}/32
 bundle exec cap production deploy
-aws ec2 revoke-security-group-ingress --group-id ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${IP}}/32
+aws ec2 revoke-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${IP}}/32
