cfef6b5bfbf16fbe4b0fc8f8cb88d67fd3ed51f4
to-Kenji <kura101089kenji@gmail.com>
2021-04-06 16:04:11 +0900

    deploy.shのタイポ修正

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ set -ex
 SECURITY_GROUP="sg-03f33e0d66dfbc166"
 IP=`curl -s ifconfig.me`
 
-aws ec2 authorize-security-group-ingress --group-id ${SECURITY_GROUP} --protocol tcp --port 22 --cidr ${IP}}/32
+aws ec2 authorize-security-group-ingress --group-id ${SECURITY_GROUP} --protocol tcp --port 22 --cidr ${IP}/32
 bundle exec cap production deploy
-aws ec2 revoke-security-group-ingress --group-id ${SECURITY_GROUP} --protocol tcp --port 22 --cidr ${IP}}/32
+aws ec2 revoke-security-group-ingress --group-id ${SECURITY_GROUP} --protocol tcp --port 22 --cidr ${IP}/32
